5ba58ede3d06bd07c30e4c86e99050b88c6dc8cd
“pandatofu” <liulinTiger@163.com>
2023-04-02 17:29:11 +0800

    Replace project name

--- book_system/book_system/asgi.py
+++ book_system/book_system/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for book_backend project.
+ASGI config for book_system project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "book_backend.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "book_system.settings")
 
 application = get_asgi_application()
